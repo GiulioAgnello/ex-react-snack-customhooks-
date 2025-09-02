0277e905482c8e00dccf2e26817abeee070e1ab3
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-09-02 18:06:39 +0200

    fixsneck 1

--- src/CustomHook/useSwitch.jsx
+++ src/CustomHook/useSwitch.jsx
@@ -1,15 +1,10 @@
 import { useState } from "react";
 
-export default function useSwitc() {
-  const [isOn, setIsOn] = useState(false);
+export default function useSwitc(initialValue = false) {
+  const [isOn, setIsOn] = useState(initialValue);
 
   function toggle() {
-    if (isOn === false) {
-      setIsOn(true);
-    }
-    if (isOn === true) {
-      setIsOn(false);
-    }
+    setIsOn((curr) => !curr);
   }
   return [isOn, toggle];
 }
